d98c88b33a60d812455125d40de5a8bba4bf4216
iddealist <illdream@hotmail.com>
2025-04-05 12:30:45 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#1e293b', color: 'white', height: '100vh', padding: '2rem' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Bienvenido a tu tablero RPG</h1>
+      <p>¡Esta es una versión de prueba para comprobar que todo funciona!</p>
+    </div>
+  );
+}
